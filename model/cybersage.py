b2b86b59486135ccaf00c3c0d2dae4ee04dc533e
research-simulation26 <research-simulation26@hf.co>
2026-06-11 08:15:35 +0000

    Add skip connection to match checkpoint

--- model/cybersage.py
+++ model/cybersage.py
@@ -17,6 +17,7 @@ class CyberSAGE(nn.Module):
             BatchNorm(hidden_channels),
             BatchNorm(hidden_channels),
         ])
+        self.skip = nn.Linear(in_channels, hidden_channels)
         self.classifier = nn.Sequential(
             nn.Linear(hidden_channels, 64),
             nn.ReLU(),
@@ -26,10 +27,11 @@ class CyberSAGE(nn.Module):
         self.dropout = dropout
 
     def forward(self, x, edge_index):
+        s = self.skip(x)
         for conv, bn in zip(self.convs, self.bns):
             x = F.relu(bn(conv(x, edge_index)))
             x = F.dropout(x, p=self.dropout, training=self.training)
-        return self.classifier(x)
+        return self.classifier(x + s)
 
 
 class EmbeddingExtractor(nn.Module):
@@ -38,7 +40,8 @@ class EmbeddingExtractor(nn.Module):
         self.model = model
 
     def forward(self, x, edge_index):
+        s = self.model.skip(x)
         for conv, bn in zip(self.model.convs, self.model.bns):
             x = F.relu(bn(conv(x, edge_index)))
             x = F.dropout(x, p=0.0, training=False)
-        return x
+        return x + s
